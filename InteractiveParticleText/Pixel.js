9e0d5689af277ee8899f1f82b8c8382a3d18b046
o00Jaiger00o <weirtrenton@gmail.com>
2021-04-12 20:49:53 -0400

    added pixeled letters
    Made it where the letter selected changed into pixels and lines.

--- InteractiveParticleText/Pixel.js
+++ InteractiveParticleText/Pixel.js
@@ -8,7 +8,7 @@ let particleArray = [];
 const mouse = {
   x:null,
   y:null,
-  radius: 250
+  radius: 100
 }
 
 window.addEventListener('mousemove', function(event){
@@ -18,25 +18,25 @@ window.addEventListener('mousemove', function(event){
 });
 
 ctx.fillStyle = 'white';
-ctx.font = '30px Verdana';
-ctx.fillText('A', 0, 30);
+ctx.font = '60px Verdana';
+ctx.fillText(':)', 0, 80);
 /*Creates rectange for visual area
 ctx.strokeStyle = 'white';
 ctx.strokeRect(0, 0, 100, 100);*/
-const data = ctx.getImageData(0, 0, 100, 100);
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
 
 class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = 3;
+    this.size = 2;
     this.baseX = this.x;
     this.baseY = this.y;
     this.density = (Math.random() * 200) + 3;
   }
   draw(){
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'white';
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.closePath();
@@ -57,14 +57,16 @@ class Particle {
     if (distance < mouse.radius){
       this.x -= directionX;
       this.y -= directionY;
-    }else{
+    }
+
+    else{
       if(this.x !== this.baseX){
         let dx= this.x - this.baseX
         this.x -= dx/10;
       }
       if(this.x !== this.baseY){
         let dy = this.y -this.baseY;
-        this.y -=dy/10;
+        this.y -= dy/10;
       }
     }
   }
@@ -74,8 +76,8 @@ class Particle {
 function init(){
   particleArray = [];
   for(let y = 0, y2 = textCoordinates.height; y < y2; y++){
-      for(let x = 0, x2 = textCoordinates.width: x < x2; x++){
-        if(textCoordinates.data[(y * 4 * textCoordinates.width) +(x*4) + 3] > 128){
+      for(let x = 0, x2 = textCoordinates.width; x < x2; x++){
+        if(textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128){
           let positionX = x;
           let positionY = y;
           particleArray.push(new Particle(positionX * 10, positionY * 10));
@@ -83,7 +85,7 @@ function init(){
       }
   }
 
-/*                              original particle display with random particles
+/*                             //original particle display with random particles
   for(let i = 0; i < 1000; i++){
     let x = Math.random() *canvas.width;
     let y = Math.random() * canvas.height;
@@ -102,6 +104,29 @@ function animate(){
     particleArray[i].draw();
     particleArray[i].update();
   }
+  connect();
   requestAnimationFrame(animate);
 }
 animate();
+
+function connect(){
+  for(let a = 0; a < particleArray.length; a++){
+    for(let b = a; b < particleArray.length; b++){
+      /*let dx = mouse.x - this.x;
+      let dy = mouse.y - this.y;
+      let distance = Math.sqrt(dx * dx + dy * dy);*/
+      let dx = particleArray[a].x - particleArray[b].x;
+      let dy = particleArray[a].y - particleArray[b].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < 15){
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = .5;
+        ctx.beginPath();
+        ctx.moveTo(particleArray[a].x, particleArray[a].y);
+        ctx.lineTo(particleArray[b].x, particleArray[b].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
